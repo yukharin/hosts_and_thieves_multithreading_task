3032551cff7fdadff724c4d8f45c560269c66ea4
vlad_yukharin <yukharinvlad@gmail.com>
2019-05-23 14:36:05 +0500

    Hello world with an input from a stdin and with simple checking with a regex

--- src/main/java/com/yukharin/Main.java
+++ src/main/java/com/yukharin/Main.java
@@ -4,19 +4,21 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
         System.out.println("Hello world " + inputNameFromStdin());
     }
 
     public static String inputNameFromStdin() {
         System.out.println("Please enter your name here");
         String input = null;
+        String regex = "^[a-zA-Z]+$";
         try (Scanner scanner = new Scanner(System.in)) {
             input = scanner.nextLine();
-            if (input.isEmpty() || input == null) {
-                System.out.println("Name can not be empty");
-                inputNameFromStdin();
+            while (input.isEmpty() || input == null || !input.matches(regex)) {
+                System.out.println("Name can not be empty and should have only an uppercase and lowercase characters");
+                input = scanner.nextLine();
             }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
         return input;
     }
