cc6cf0383f91fb1fb9f0d68106d98336a2fc8413
vlad_yukharin <yukharinvlad@gmail.com>
2019-05-23 14:18:28 +0500

    Hello world with an input from a stdin

--- src/main/java/com/yukharin/Main.java
+++ src/main/java/com/yukharin/Main.java
@@ -1,8 +1,23 @@
 package com.yukharin;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
-        System.out.println("For testing purposes");
+
+        System.out.println("Hello world " + inputNameFromStdin());
+    }
+
+    public static String inputNameFromStdin() {
+        System.out.println("Please enter your name here");
+        String input = null;
+        try (Scanner scanner = new Scanner(System.in)) {
+            input = scanner.nextLine();
+            if (input.isEmpty() || input == null) {
+                System.out.println("Name can not be empty");
+                inputNameFromStdin();
+            }
+        }
+        return input;
     }
 }
