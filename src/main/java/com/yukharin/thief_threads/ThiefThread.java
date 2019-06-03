89d9d9a371e0740655f77ad814eba922dfbab6de
vlad_yukharin <yukharinvlad@gmail.com>
2019-06-03 14:39:22 +0500

    Small refactoring

--- src/main/java/com/yukharin/thief_threads/ThiefThread.java
+++ src/main/java/com/yukharin/thief_threads/ThiefThread.java
@@ -1,20 +1,28 @@
 package com.yukharin.thief_threads;
 
+import com.yukharin.homes.Home;
 import com.yukharin.thieves.Thief;
 
 import java.util.concurrent.BrokenBarrierException;
 import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.Semaphore;
 
 public class ThiefThread implements Runnable {
 
     private Thief thief;
     private CyclicBarrier start;
     private CyclicBarrier finish;
+    private final int permits;
+    private Home home;
+    private Semaphore semaphore;
 
-    public ThiefThread(Thief thief, CyclicBarrier start, CyclicBarrier finish) {
+    public ThiefThread(Thief thief, Home home, CyclicBarrier start, CyclicBarrier finish, Semaphore semaphore, int permits) {
         this.thief = thief;
         this.start = start;
         this.finish = finish;
+        this.home = home;
+        this.semaphore = semaphore;
+        this.permits = permits;
     }
 
     @Override
@@ -22,9 +30,9 @@ public class ThiefThread implements Runnable {
         if (start != null) {
             try {
                 start.await();
-                synchronized (thief.getHome()) {
-                    thief.steal();
-                }
+                semaphore.acquire(permits);
+                thief.steal(home);
+                semaphore.release(permits);
                 finish.await();
             } catch (InterruptedException | BrokenBarrierException e) {
                 e.printStackTrace();
