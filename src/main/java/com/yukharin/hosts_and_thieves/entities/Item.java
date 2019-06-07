60b5cedbff3bb7e98e9b8882ee742627ce3d24be
vlad_yukharin <yukharinvlad@gmail.com>
2019-06-07 16:05:10 +0500

    Added small feature

--- src/main/java/com/yukharin/hosts_and_thieves/entities/Item.java
+++ src/main/java/com/yukharin/hosts_and_thieves/entities/Item.java
@@ -4,7 +4,6 @@ public class Item implements Comparable<Item> {
 
     private int weight;
     private int value;
-    public static final Item EMPTY_ITEM = new Item(0, 0);
 
     public Item(int weight, int value) {
         this.weight = weight;
