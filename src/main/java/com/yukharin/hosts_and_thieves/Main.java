b255225a8e55d64dc40567300d6b711e5c3e5eb5
vlad_yukharin <yukharinvlad@gmail.com>
2019-06-13 17:19:59 +0500

    Added some Improvements

--- src/main/java/com/yukharin/hosts_and_thieves/Main.java
+++ src/main/java/com/yukharin/hosts_and_thieves/Main.java
@@ -10,7 +10,6 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -20,36 +19,33 @@ public class Main {
 
     private static final int HOSTS = 100;
     private static final int THIEVES = 100;
-    private static final int ITEMS_PER_HOST = 5;
+    private static final int ITEMS_PER_HOST = 15;
     private static final int TOTAL_THREADS = HOSTS + THIEVES;
     private static final Semaphore semaphore = new Semaphore(HOSTS);
     private static final Home home = new Home();
-    private static final Runnable task = () -> {
-        Utils.printInfo(home);
-        Thread.currentThread().setPriority(Thread.MIN_PRIORITY);
-    };
-    private static final CyclicBarrier barrier = new CyclicBarrier(TOTAL_THREADS, task);
+    private static final CyclicBarrier barrier = new CyclicBarrier(TOTAL_THREADS);
     private static final AtomicInteger threadsCounter = new AtomicInteger();
     private static final Logger logger = LogManager.getLogger(Main.class);
+    private static final int TIMEOUT = 3;
 
 
     public static void main(String[] args) throws InterruptedException {
         long startingTime = System.currentTimeMillis();
-        logger.info("Starting time: " + startingTime);
         List<Runnable> threads = new ArrayList<>(TOTAL_THREADS);
-        ExecutorService service = Executors.newFixedThreadPool(TOTAL_THREADS);
         for (int i = 0; i < HOSTS; i++) {
             threads.add(new HostThread(new Host(ITEMS_PER_HOST), home, semaphore, barrier, threadsCounter));
         }
         for (int i = 0; i < THIEVES; i++) {
             threads.add(new ThiefThread(new Thief(), home, semaphore, HOSTS, barrier, threadsCounter));
         }
-        Collections.shuffle(threads);
+        Utils.printInfo(home);
+        ExecutorService service = Executors.newFixedThreadPool(TOTAL_THREADS);
         for (Runnable runnable : threads) {
             service.submit(runnable);
         }
         service.shutdown();
-        service.awaitTermination(3, TimeUnit.MINUTES);
+        service.awaitTermination(TIMEOUT, TimeUnit.MINUTES);
+        Utils.printInfo(home);
         long endingTime = System.currentTimeMillis();
         logger.info("Ending time: " + endingTime);
         logger.info("Perfomance: " + (endingTime - startingTime) + " millis");
